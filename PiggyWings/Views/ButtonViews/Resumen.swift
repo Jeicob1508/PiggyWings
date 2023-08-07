dcd0d6df1e69765bd521c5aead69c70ca36262eb
Jacobo Osorio <jacobood1508@gmail.com>
2023-08-06 19:34:45 -0500

    Update

--- PiggyWings/Views/ButtonViews/Resumen.swift
+++ PiggyWings/Views/ButtonViews/Resumen.swift
@@ -9,11 +9,26 @@ import SwiftUI
 
 struct ResumenView: View {
     @EnvironmentObject var globalState: GlobalState
+    @Environment(\.managedObjectContext) private var moc
+    @State private var mostrar: Bool = false
 
     var body: some View {
     
         VStack{
             Text("Ventana del Resumen")
+            HStack{
+                Button(action:{
+                    self.mostrar.toggle()
+                }){
+                    Image(systemName: "gearshape.fill")
+                }
+                .font(.system(size: 30))
+                .padding()
+                .sheet(isPresented: self.$mostrar, content: {
+                    Configuracion().environment(\.managedObjectContext, self.moc)
+                })
+                Spacer()
+            }
             Spacer()
             Text("Actualmente tienes: \(String(format: "%.2f", globalState.money))")
                 .padding()
---
+++ PiggyWings/Views/Configuracion.swift
@@ -0,0 +1,20 @@
+//
+//  Configuracion.swift
+//  PiggyWings
+//
+//  Created by Jacobo Osorio on 6/08/23.
+//
+
+import SwiftUI
+
+struct Configuracion: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Configuracion_Previews: PreviewProvider {
+    static var previews: some View {
+        Configuracion()
+    }
+}
